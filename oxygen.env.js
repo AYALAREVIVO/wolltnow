86a933c3eb66486fb0835d5d83b4a79aeddbba27
ayala revivo <ar0527167617@gmail.com>
2023-05-23 08:54:54 +0300

    Update oxygen.env.js

--- oxygen.env.js
+++ oxygen.env.js
@@ -1,20 +1,8 @@
 module.exports = {
-
-    TEST: { 
-       urlHomePage:'https://www.w3schools.com/',
-        urlJavaT:'https://www.w3schools.com/java/default.asp',
-    urlJavaqI:'https://www.w3schools.com/java/java_intro.asp',
-    urlHTMLT:'https://www.w3schools.com/html/default.asp',
-    urlHTMLTable:'https://www.w3schools.com/html/html_tables.asp'
+    default: {
+        baseUrl: "https://automationbookstore.dev/"
     },
-
-    PROD: {
-             urlHomePage:'https://www.w3schools.com/',
-        urlJavaT:'https://www.w3schools.com/java/default.asp',
-    urlJavaqI:'https://www.w3schools.com/java/java_intro.asp',
-    urlHTMLT:'https://www.w3schools.com/html/default.asp',
-    urlHTMLTable:'https://www.w3schools.com/html/html_tables.asp'
-       
-    },
-
-}
+    prod: {
+        baseUrl: "http://prod.automationbookstore.dev"
+    }
+};
